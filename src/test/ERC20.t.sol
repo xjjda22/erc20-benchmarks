2ffcdffae817e8685189d1fa55b2060efa020a82
harryranakl <ranahari@gmail.com>
2022-04-19 17:52:43 +0800

    updated:

--- src/test/ERC20.t.sol
+++ src/test/ERC20.t.sol
@@ -14,29 +14,42 @@ contract ERC20Test is DSTest {
     address bob  = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
 
     function setUp() public {
+        console.log('setUp',msg.sender);
         initialAccount = msg.sender;
         m20 = new ERC20Mock('ERC20MOCKBENCH','E20B', msg.sender, 1e18);
     }
 
     function testName() public {
+        console.log('testName',msg.sender);
         assertEq(m20.name(),'ERC20MOCKBENCH');
     }
 
     function testSymbol() public {
+        console.log('testSymbol',msg.sender);
         assertEq(m20.symbol(),'E20B');
     }
 
     function testSupply() public {
+        console.log('testSupply',msg.sender);
         assertEq(m20.totalSupply(),1e18);
     }
 
     function testBalance() public {
+        console.log('testBalance',msg.sender);
         assertEq(m20.balanceOf(initialAccount), 1e18);
     }
 
-    function testMint() public {
-        assertEq(m20.balanceOf(initialAccount), 1e18);
-        m20.mint(initialAccount, 100);
-        assertGt(m20.balanceOf(initialAccount), 1e18);
+    function testMintAlice() public {
+        console.log('testMint',msg.sender);
+        assertEq(m20.balanceOf(alice), 0);
+        m20.mint(alice, 100);
+        assertGt(m20.balanceOf(alice), 99);
+    }
+
+    function testMintBob() public {
+        console.log('testMint',msg.sender);
+        assertEq(m20.balanceOf(bob), 0);
+        m20.mint(bob, 10000);
+        assertEq(m20.balanceOf(bob), 10000);
     }
 }
